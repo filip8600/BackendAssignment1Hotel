1317351e195f0e7938c00e21469bcc4133e67366
auAnnSo <64138186+auAnnSo@users.noreply.github.com>
2022-02-18 10:26:10 +0100

    Co-authored-by: Kat1606 <Kat1606@users.noreply.github.com> Co-authored-by: filip8600 <filip8600@users.noreply.github.com>

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,17 @@
 const bodyParser = require('body-parser');
-
 const express = require('express')
+import https from 'https';
+import helmet from 'helmet';
+import fs from 'fs';
+
 const app = express()
 const port = 3001
+
+const options =  {
+  key: fs.readFileSync(path.join(__dirname, '../selfsigned.key')),
+  cert: fs.readFileSync(path.join(__dirname, '../selfsigned.crt'))
+};
+app.use(helmet());
 
 app.use(bodyParser.json())
 app.use('/orders', require('./controllers/plainController'));
@@ -10,6 +19,6 @@ app.get('/hello', (req: any, res: any) => {
   res.send('Hello World!')
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+https.createServer(options, app).listen(port, () => {
+  console.log(`Running 'secure-http' on ${port}`);
+});
