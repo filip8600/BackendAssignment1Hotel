96e1d1667d24a4cfb6cc023ceba2ca673b687ecb
Filip <f-r-c@LAPTOP-BO73C4EV>
2022-02-25 22:00:39 +0100

    Lint

--- src/Controllers/reservationController.ts
+++ src/Controllers/reservationController.ts
@@ -3,7 +3,7 @@ const express = require('express')
 import { Request, Response } from 'express'
 import mongoose from 'mongoose'
 import { ireservation, reservations_Schema } from '../Models/reservation_Schema'
-import {getRole,getEmail} from "./authenticationController"
+import { getRole, getEmail } from "./authenticationController"
 
 
 export const reservationsController = express.Router();
@@ -19,56 +19,76 @@ Reservations
   - `DELETE /reservations/{:uid}`–delete reservation. Accessible for roles `manager`, `clerk`
 
 */
-
+//https://localhost:3001/reservations?from=20221010?to=20221111
 const read = async (req: Request, res: Response) => {
-if(getRole(req)!=='manager'&&getRole(req)!=='clerk') { return res.status(400).send("Not correct Role")}
-  let result = await reservationsModel.find({}, { __v: 0}).sort('reservationStart').lean().exec()
-  res.json(result)
+    if (getRole(req) !== 'manager' && getRole(req) !== 'clerk') { return res.status(400).send("Not correct Role") }
+    //Build filter
+    let filter = {}
+    let start = req.query.from //https://stackoverflow.com/a/17008027
+    if (start) {
+        let startDate = new Date(start.toString())
+        filter = { reservationStart: { $gt: startDate } }//Virker IKKE
+    }
+
+    let end = req.query.to
+    if (end) {
+        let endDate = new Date(end.toString())
+        filter = { ...filter, reservationEnd: { $lt: endDate } }//Virker IKKE
+    }
+    //Perform querry:
+    let result = await reservationsModel.find({ filter }, { __v: 0 }).sort('reservationStart').lean().exec()
+    //return res.json(filter)
+
+    res.json(result)
 }
 
-const create =  async (req: Request, res: Response) => {
-  let filters= { room_number: {$eq: req.body.room_number},
-                reservationEnd:{$gt: req.body.reservationStart},
-                reservationStart:{$lt: req.body.reservationEnd}}
-
-let conflicts=await reservationsModel.find(filters).lean().exec()
-    if(conflicts.length<1) {
-        let { id } = await new reservationsModel(req.body).save()
-        res.json(id )
+const create = async (req: Request, res: Response) => {
+    let filters = {
+        room_number: { $eq: req.body.room_number },
+        reservationEnd: { $gt: req.body.reservationStart },
+        reservationStart: { $lt: req.body.reservationEnd }
     }
-    else(res.status(400).json(conflicts))
+
+    let conflicts = await reservationsModel.find(filters).lean().exec()
+    if (conflicts.length < 1) {
+        let { id } = await new reservationsModel(req.body).save()
+        res.json(id)
+    }
+    else (res.status(400).json(conflicts))
 }
 
 const readOne = async (req: Request, res: Response) => {
-  const { uid } = req.params
-  let result:ireservation = await reservationsModel.findOne({ _id: uid }, { __v: 0}).exec()
-  if(getRole(req)!=='manager'&&getRole(req)!=='clerk') { 
-      if(getEmail(req)!==result.customerEmail) {return res.status(400).send(getEmail(req)+ " != " + result.customerEmail)}}//result.customerEmail er noget lort
-//else:
-  res.json(result)
+    const { uid } = req.params
+    let result: ireservation = await reservationsModel.findOne({ _id: uid }, { __v: 0 }).exec()
+    if (getRole(req) !== 'manager' && getRole(req) !== 'clerk') {
+        if (getEmail(req) !== result.customerEmail) { return res.status(400).send(getEmail(req) + " != " + result.customerEmail) }
+    }//result.customerEmail er noget lort
+    //else:
+    res.json(result)
 }
 
 
 
 const update = async (req: Request, res: Response) => {
-  const { uid } = req.params
-  const body = req.body
-  let result = await reservationsModel.updateOne({_id: uid }, { $set: body }).exec()
-  if(getRole(req)!=='manager' && getRole(req)!=='clerk') { 
-    if(getEmail(req)!==result.customerEmail) {return res.status(400).send("This is not your reservation")}}
-  res.json({uid, result})
+    const { uid } = req.params
+    const body = req.body
+    let result = await reservationsModel.updateOne({ _id: uid }, { $set: body }).exec()
+    if (getRole(req) !== 'manager' && getRole(req) !== 'clerk') {
+        if (getEmail(req) !== result.customerEmail) { return res.status(400).send("This is not your reservation") }
+    }
+    res.json({ uid, result })
 }
 
 const remove = async (req: Request, res: Response) => {
-    if(getRole(req)!=='manager'&&getRole(req)!=='clerk') { return res.status(400).send("Not correct Role")}
+    if (getRole(req) !== 'manager' && getRole(req) !== 'clerk') { return res.status(400).send("Not correct Role") }
     const { uid } = req.params
     let result = await reservationsModel.deleteOne({ _id: uid })
     res.json(result)
 }
-export const reservation ={
-  read,
-  readOne,
-  create,
-  update,
-  remove
+export const reservation = {
+    read,
+    readOne,
+    create,
+    update,
+    remove
 }
